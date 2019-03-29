8f868f2360b7cecf122b0ffebb3c31af3295f3f1
Wade Pines <wrp5031@conrad.cis.rit.edu>
2019-03-29 19:45:28 -0400

    --scansheets   --Included a jpg version of the blank scan tron sheet so it can be opened in OpenCV --ScanThatTronGitHub.cpp   --Added some more image pre-processing steps     --Gaussian Blur     --Adaptive Thresholding

--- ScanThatTronGitHub.cpp
+++ ScanThatTronGitHub.cpp
@@ -1,6 +1,6 @@
 #include <ctime>
 #include <iostream>
-//hi
+
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <opencv2/core.hpp>
@@ -51,25 +51,57 @@ int main(int argc, char* argv[]) {
     cout << "Channels: " << src.channels() << endl;
   }
   
-  /**************************************** Main Code and what not *********************************/
-  
   clock_t startTime = clock();
 
-  // Convert to grayscale is color
+  /**************************************** Main Code and what not *********************************/
+
+  // Convert to grayscale if color
+  cv::Mat gray = src;
   if(src.channels() > 1){
-  cv::Mat g;
-  cv::cvtColor(src, g, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
   }
+  
+  // Add a Gaussian Blur to gray image
+  // General 3 by 3 gaussian blur filter used (Maybe look into increasing kernel size?)(3 by 3 is pretty subtle)
+  cv::Mat gaussFilter;
+  cv::Mat gauss;
+
+  gaussFilter.create(3, 3, CV_32FC1);
+  gaussFilter.at<float>(0, 0) = 0.0113;
+  gaussFilter.at<float>(1, 0) = 0.0838;
+  gaussFilter.at<float>(2, 0) = 0.0113;
+
+  gaussFilter.at<float>(0, 1) = 0.0838;
+  gaussFilter.at<float>(1, 1) = 0.6193;
+  gaussFilter.at<float>(2, 1) = 0.0838;
+
+  gaussFilter.at<float>(0, 2) = 0.0113;
+  gaussFilter.at<float>(1, 2) = 0.0838;
+  gaussFilter.at<float>(2, 2) = 0.0113;
+
+  cv::filter2D(gray, gauss, CV_32FC1, gaussFilter); //Convolve gray image with gaussian filter
+  
+  // Apply adaptive thresholding (NOTE: The blank scan tron sheet is technically already binary!)
+  cv::Mat binary;
+  gauss.convertTo(gauss,src.type()); 
+  
+  cv::adaptiveThreshold(gauss, binary, 255, cv::ADAPTIVE_THRESH_MEAN_C,cv::THRESH_BINARY,7, 7); //3rd parameter scales binary mat to 0s and 255s
+  
+  /*************************************************************************************************/
   
   clock_t endTime = clock();
 
-  /****************************************************************************************************************/
   if (verbose) {
     cout << "Elapsed time: "
          << (endTime - startTime) / static_cast<double>(CLOCKS_PER_SEC)
          << " [s]" << endl;
   }
+  
+  cv::namedWindow("src",cv::WINDOW_NORMAL); 
+  cv::resizeWindow("src", 800,800); //Cause this image is fooking huuugggee
+  cv::imshow("src",binary);
+  cv::waitKey(0);
+
 
   return EXIT_SUCCESS;
 }
-
