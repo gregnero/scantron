dae1eebb7884d071c80f7a87228bb8a030914b85
Wade Pines <wrp5031@conrad.cis.rit.edu>
2019-04-05 18:36:20 -0400

    New code. You can look at the old code in the github history. --This code is meant for the vector pdf blank scan sheet   --Takes advantage of Hough Circles     --Perfectly circles all bubbles on the scan tron plus a few outliers       --Radius of the bubbles are all the same though   --Circles that are not equal to the majority radius(aka. the bubble radius) are discarded   --A new vector of points is made to save just the center location of the circles     --We do not need to record the bubble radius because that is constant for all bubbles   --Hough Circles sucks and feels the need to randomly place the circle locations in the vector     --The vector of points that contains the column and row location of each bubble is sorted from top to bottom and left to right

--- ScanThatTronGitHub.cpp
+++ ScanThatTronGitHub.cpp
@@ -11,6 +11,13 @@ using namespace std;
 
 namespace po = boost::program_options;
 
+
+struct myclass {
+    bool operator() (cv::Point pt1, cv::Point pt2) {     
+    return (pt1.x + (1000*pt1.y)) < (pt2.x + (1000*pt2.y)); //This is going to sort the bubble vector from top to bottom and left to right
+    }
+} myobject;
+
 int main(int argc, char* argv[]) {
   bool verbose = false;
   string src_filename = "";
@@ -61,52 +68,60 @@ int main(int argc, char* argv[]) {
     cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
   }
   
-  // Add a Gaussian Blur to gray image
-  // General 3 by 3 gaussian blur filter used (Maybe look into increasing kernel size?)(3 by 3 is pretty subtle)
-  cv::Mat gaussFilter;
-  cv::Mat gauss;
+  vector<cv::Vec3f> circles;
 
-  gaussFilter.create(3, 3, CV_32FC1);
-  gaussFilter.at<float>(0, 0) = 0.0113;
-  gaussFilter.at<float>(1, 0) = 0.0838;
-  gaussFilter.at<float>(2, 0) = 0.0113;
+  //Apply the Hough Transform to find the circles
+  //Randomly tried a bunch of different threshold values and these seemed to work out ok
+  cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, src.rows/100, 200, 50, 0, src.rows/50); //src.rows/100, 200, 60, 0, src.rows/50);
+  
+  vector<cv::Vec3f> bubbles(1450); //1450 because that is the number of bubbles in our scantron
+                                   //Regular Hough Circles gives us some circles that we do not care about
+                                   //This vector will hold only the bubble circles
+  vector<cv::Point> bubbleXY;
+                                   
+  int c = 0; //Used to fill our bubbles vector in the loop
+  for( size_t i = 0; i < circles.size(); i++ ){
 
-  gaussFilter.at<float>(0, 1) = 0.0838;
-  gaussFilter.at<float>(1, 1) = 0.6193;
-  gaussFilter.at<float>(2, 1) = 0.0838;
+      cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
 
-  gaussFilter.at<float>(0, 2) = 0.0113;
-  gaussFilter.at<float>(1, 2) = 0.0838;
-  gaussFilter.at<float>(2, 2) = 0.0113;
+      if(radius != cvRound(circles[20][2])){ //This is just so we fill our new vector properly
+        c = c - 1;
+      }
 
-  cv::filter2D(gray, gauss, CV_32FC1, gaussFilter); //Convolve gray image with gaussian filter
+      if(radius == cvRound(circles[20][2])){ //99.99% of the time this will be the value
+                                             //The radius of all the bubble circles will be the same
+                                             //given the blank scan tron image is of extremely high resolution
+                                             //If we come across a circle that has the same radius as the other
+                                             //bubble circles lets add that to our bubble circle vector       
+        
+        bubbles[c][0] = cvRound(circles[i][0]); //columns
+        bubbles[c][1] = cvRound(circles[i][1]); //rows
+        bubbles[c][2] = cvRound(circles[i][2]); //radius
+        
+        bubbleXY.push_back(cv::Point(cvRound(circles[i][0]), cvRound(circles[i][1])));
+        
+      }
+      c++;
+   }
+
+  std::sort(bubbleXY.begin(), bubbleXY.end(), myobject);
   
-  // Apply global threhsolding (NOTE: The blank scan tron sheet is technically already binary!)
-  cv::Mat binary;
-  gauss.convertTo(gauss,src.type()); 
-  
-  cv::Mat stdDev;
-  cv::Mat mean;
-  cv::meanStdDev(src, mean, stdDev);
-  double T = 255 - stdDev.at<double>(0,0); //Clip/threshold value (This threshold value is experimental)
-  
-  cv::threshold(gauss,binary,T,255,cv::THRESH_BINARY_INV); //Inverse is taken so it works with morphology 
-  
-  //Morphology
-    //Dilation (Strengthens the bubbled circles)
-  cv::Mat dilate;
-  
-  cv::Mat kernelDilate = cv::getStructuringElement(cv::MORPH_DILATE, cv::Point(9,9));
-  cv::dilate(binary, dilate, kernelDilate);
-  
-    //Erosion (Removes noise)
-  cv::Mat erode;
-  
-  cv::Mat kernelErosion = cv::getStructuringElement(cv::MORPH_ERODE, cv::Point(3,3));
-  cv::erode(dilate, erode, kernelErosion);
-  
-  erode = ~erode; //Invert the image
-  
+  for(int i = 0; i < bubbles.size(); i++){
+
+      int radius = cvRound(bubbles[i][2]);
+      cv::circle(src, bubbleXY[i], 3, cv::Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      cv::circle(src, bubbleXY[i], radius, cv::Scalar(0,0,255), 3, 8, 0 ); 
+      
+      //Moved the imshow stuff here temporarily for visulation
+      cv::namedWindow("src",cv::WINDOW_NORMAL); 
+      cv::resizeWindow("src", 800,800); //Cause this image is fooking huuugggee
+      cv::imshow("src",src);
+      cv::waitKey(1);
+                     
+  }
+ 
   /*************************************************************************************************/
   
   clock_t endTime = clock();
@@ -116,15 +131,11 @@ int main(int argc, char* argv[]) {
          << (endTime - startTime) / static_cast<double>(CLOCKS_PER_SEC)
          << " [s]" << endl;
   }
-  
-  cv::namedWindow("src",cv::WINDOW_NORMAL); 
-  cv::resizeWindow("src", 800,800); //Cause this image is fooking huuugggee
-  cv::imshow("src",src);
-  
-  cv::namedWindow("erode",cv::WINDOW_NORMAL); 
-  cv::resizeWindow("erode", 800,800); //Cause this image is fooking huuugggee
-  cv::imshow("erode",erode);
-  cv::waitKey(0);
+    
+  //cv::namedWindow("src",cv::WINDOW_NORMAL); 
+  //cv::resizeWindow("src", 800,800); //Cause this image is fooking huuugggee
+  //cv::imshow("src",src);
+  //cv::waitKey(0);
 
 
   return EXIT_SUCCESS;
